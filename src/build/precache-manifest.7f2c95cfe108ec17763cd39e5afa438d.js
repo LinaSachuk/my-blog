8b5a78785e6e6818eb30a680d9ad63ef385654f4
Lina Sachuk <sachuklina@gmail.com>
2020-09-27 22:51:56 -0600

    first commit

---
+++ src/build/precache-manifest.7f2c95cfe108ec17763cd39e5afa438d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5fddae575ed2947e562",
+    "url": "/static/css/main.266ea6e5.chunk.css"
+  },
+  {
+    "revision": "d5fddae575ed2947e562",
+    "url": "/static/js/main.5c50a4fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18276a6e48ef64182dcb",
+    "url": "/static/js/2.fecbef94.chunk.js"
+  },
+  {
+    "revision": "d77200a922a889e7b90c71153518f61b",
+    "url": "/index.html"
+  }
+];
